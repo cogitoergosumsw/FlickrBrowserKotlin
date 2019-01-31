88d8ac536640c19241cb45e64aa7bd994e1a3c78
Seng Wee <>
2019-01-31 12:11:24 +0800

    download JSON and doInBackground

---
+++ app/src/main/java/com/skyblue/learn/flickrbrowser_kotlin/GetRawData.kt
@@ -0,0 +1,56 @@
+package com.skyblue.learn.flickrbrowser_kotlin
+
+import android.os.AsyncTask
+import android.util.Log
+import java.io.IOException
+import java.net.MalformedURLException
+import java.net.URL
+
+/*
+*   Created by Seng Wee on
+*   31/1/19
+*/
+enum class DownloadStatus {
+    OK, IDLE, NOT_INITIALISED, FAILED_OR_EMPTY, PERMISSIONS_ERROR, ERROR
+}
+class GetRawData: AsyncTask<String, Void, String>() {
+    private val TAG = "GetRawData"
+    private var downloadStatus = DownloadStatus.IDLE
+
+    override fun onPostExecute(result: String?) {
+        super.onPostExecute(result)
+    }
+
+    override fun doInBackground(vararg params: String?): String {
+        if (params[0] == null) {
+            downloadStatus = DownloadStatus.NOT_INITIALISED
+            return "no URL specified"
+        }
+
+        try {
+            downloadStatus = DownloadStatus.OK
+            return URL(params[0]).readText()
+        } catch (e: Exception) {
+            val errorMessage = when (e) {
+                is MalformedURLException -> {
+                    downloadStatus = DownloadStatus.NOT_INITIALISED
+                    "doInBackground: Invalid URL ${e.message}"
+                }
+                is IOException -> {
+                    downloadStatus = DownloadStatus.FAILED_OR_EMPTY
+                    "doInBackground: IOException reading data: ${e.message}"
+                }
+                is SecurityException -> {
+                    downloadStatus = DownloadStatus.PERMISSIONS_ERROR
+                    "doInBackground: Need permissions? ${e.message}"
+                }
+                else -> {
+                    downloadStatus = DownloadStatus.ERROR
+                    "doInBackground: Unknown error: ${e.message}"
+                }
+            }
+            Log.d(TAG, errorMessage)
+            return errorMessage
+        }
+    }
+}
